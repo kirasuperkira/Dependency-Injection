e3b1a8091e512433cf23ecb4d0ac9e4073aed4e5
Kira Kravchenko <91788106+kirasuperkira@users.noreply.github.com>
2025-05-01 09:21:56 +0700

    Add files via upload

---
+++ PracticalWork1/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace laba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.btnRemoveStudent = new System.Windows.Forms.Button();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.btnShowHistogram = new System.Windows.Forms.Button();
+            this.dgvStudents = new System.Windows.Forms.DataGridView();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Speciality = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Group = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddStudent
+            // 
+            this.btnAddStudent.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnAddStudent.Location = new System.Drawing.Point(51, 247);
+            this.btnAddStudent.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAddStudent.Name = "btnAddStudent";
+            this.btnAddStudent.Size = new System.Drawing.Size(210, 43);
+            this.btnAddStudent.TabIndex = 0;
+            this.btnAddStudent.Text = "Добавить студента";
+            this.btnAddStudent.UseVisualStyleBackColor = false;
+            this.btnAddStudent.Click += new System.EventHandler(this.btnAddStudent_Click);
+            // 
+            // btnRemoveStudent
+            // 
+            this.btnRemoveStudent.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnRemoveStudent.Location = new System.Drawing.Point(51, 349);
+            this.btnRemoveStudent.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRemoveStudent.Name = "btnRemoveStudent";
+            this.btnRemoveStudent.Size = new System.Drawing.Size(210, 43);
+            this.btnRemoveStudent.TabIndex = 1;
+            this.btnRemoveStudent.Text = "Удалить студента";
+            this.btnRemoveStudent.UseVisualStyleBackColor = false;
+            this.btnRemoveStudent.Click += new System.EventHandler(this.btnRemoveStudent_Click);
+            // 
+            // EditButton
+            // 
+            this.EditButton.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.EditButton.Location = new System.Drawing.Point(51, 298);
+            this.EditButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(210, 43);
+            this.EditButton.TabIndex = 2;
+            this.EditButton.Text = "Изменить данные студента";
+            this.EditButton.UseVisualStyleBackColor = false;
+            this.EditButton.Click += new System.EventHandler(this.EditButton_Click);
+            // 
+            // btnShowHistogram
+            // 
+            this.btnShowHistogram.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnShowHistogram.Location = new System.Drawing.Point(51, 400);
+            this.btnShowHistogram.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnShowHistogram.Name = "btnShowHistogram";
+            this.btnShowHistogram.Size = new System.Drawing.Size(210, 45);
+            this.btnShowHistogram.TabIndex = 3;
+            this.btnShowHistogram.Text = "Гистограмма (распределение по специальностям)";
+            this.btnShowHistogram.UseVisualStyleBackColor = false;
+            this.btnShowHistogram.Click += new System.EventHandler(this.btnShowHistogram_Click);
+            // 
+            // dgvStudents
+            // 
+            this.dgvStudents.BackgroundColor = System.Drawing.SystemColors.Menu;
+            this.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudents.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.Speciality,
+            this.Group,
+            this.Id});
+            this.dgvStudents.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgvStudents.Location = new System.Drawing.Point(48, 79);
+            this.dgvStudents.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvStudents.Name = "dgvStudents";
+            this.dgvStudents.RowHeadersWidth = 51;
+            this.dgvStudents.Size = new System.Drawing.Size(399, 106);
+            this.dgvStudents.TabIndex = 4;
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "Имя";
+            this.Name.MinimumWidth = 6;
+            this.Name.Name = "Name";
+            this.Name.Width = 140;
+            // 
+            // Speciality
+            // 
+            this.Speciality.HeaderText = "Специальность";
+            this.Speciality.MinimumWidth = 6;
+            this.Speciality.Name = "Speciality";
+            this.Speciality.Width = 125;
+            // 
+            // Group
+            // 
+            this.Group.HeaderText = "Группа";
+            this.Group.MinimumWidth = 6;
+            this.Group.Name = "Group";
+            this.Group.Width = 80;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.MinimumWidth = 6;
+            this.Id.Name = "Id";
+            this.Id.Visible = false;
+            this.Id.Width = 125;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Список студентов:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Выберете действие:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 589);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvStudents);
+            this.Controls.Add(this.btnShowHistogram);
+            this.Controls.Add(this.EditButton);
+            this.Controls.Add(this.btnRemoveStudent);
+            this.Controls.Add(this.btnAddStudent);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "DecanatPRO";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddStudent;
+        private System.Windows.Forms.Button btnRemoveStudent;
+        private System.Windows.Forms.Button EditButton;
+        private System.Windows.Forms.Button btnShowHistogram;
+        private System.Windows.Forms.DataGridView dgvStudents;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Speciality;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Group;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PracticalWork1/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using BusinessLogic;
+using Ninject;
+
+namespace laba1
+{
+    public partial class Form1: Form
+    {
+        private readonly IStudentLogic logic;
+
+        public Form1()
+        {
+            //Для управления зависимостями через Ninject создаю объект Ninject.IKernel с помощью встроенной реализации этого интерфейса - класса StandardKernel
+            IKernel ninjectKernel = new StandardKernel(new SimpleConfigModule());
+            logic = ninjectKernel.Get<IStudentLogic>();
+
+            InitializeComponent();
+            UpdateStudentList();
+        }
+
+        /// <summary>
+        /// Изменение студента
+        /// </summary>
+        private void UpdateStudentList()
+        {
+            dgvStudents.Rows.Clear();
+            var students = logic.GetAllStudents();
+            if (students != null)
+            {
+                foreach (var student in students)
+                {
+                    dgvStudents.Rows.Add(student["Name"], student["Speciality"], student["Group"], student["Id"].ToString());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Добавление студента
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAddStudent_Click(object sender, EventArgs e)
+        {
+            Form settingsForm = new Form();
+            settingsForm.FormBorderStyle = FormBorderStyle.FixedDialog;
+            settingsForm.Text = "Добавить студента";
+            settingsForm.Size = new Size(300, 210);
+            settingsForm.MaximizeBox = false;
+
+            Label nameLabel = new Label();
+            nameLabel.Text = "Имя студента: ";
+            nameLabel.Location = new Point(20, 20);
+
+            System.Windows.Forms.TextBox nameTextBox = new System.Windows.Forms.TextBox();
+            nameTextBox.Location = new Point(120, 20);
+
+            Label specLabel = new Label();
+            specLabel.Text = "Специальность студента: ";
+            specLabel.Location = new Point(20, 50);
+
+            System.Windows.Forms.TextBox specTextBox = new System.Windows.Forms.TextBox();
+            specTextBox.Location = new Point(120, 50);
+
+            Label groupLabel = new Label();
+            groupLabel.Text = "Группа студента: ";
+            groupLabel.Location = new Point(20, 80);
+
+            System.Windows.Forms.TextBox groupTextBox = new System.Windows.Forms.TextBox();
+            groupTextBox.Location = new Point(120, 80);
+
+            Button addBut = new Button();
+            addBut.Location = new Point(10, 110);
+            addBut.Text = "Добавить студента";
+
+            addBut.Click += (s, args) => {
+
+                logic.AddStudent(nameTextBox.Text, specTextBox.Text, groupTextBox.Text);
+                UpdateStudentList();
+                settingsForm.Close();
+            };
+
+            settingsForm.Controls.Add(nameLabel);
+            settingsForm.Controls.Add(nameTextBox);
+            settingsForm.Controls.Add(specLabel);
+            settingsForm.Controls.Add(specTextBox);
+            settingsForm.Controls.Add(groupLabel);
+            settingsForm.Controls.Add(groupTextBox);
+            settingsForm.Controls.Add(addBut);
+
+            settingsForm.ShowDialog();
+        }
+
+        /// <summary>
+        /// Удаление студента
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRemoveStudent_Click(object sender, EventArgs e)
+        {
+            if (dgvStudents.SelectedRows.Count > 0)
+            {
+                //Получение Id выбранного студента
+                int id;
+                if (int.TryParse(dgvStudents.SelectedRows[0].Cells["Id"].Value?.ToString(), out id))
+                {
+                    if (logic.DeleteStudent(id.ToString()))
+                    {
+                        UpdateStudentList();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Студент c данным идентификатором отсутсвует в списке.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка получения данных о студенте с данным идентификатором.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ввеедите идентификатор студента, которого вы хотите удалить.");
+            }
+        }
+
+        /// <summary>
+        /// Изменение студента
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+            if (dgvStudents.SelectedRows.Count == 1)
+            {
+                string id = dgvStudents.SelectedRows[0].Cells["Id"].Value.ToString();
+
+                Form updata = new Form() { Text = "Изменение данных о студенте", Size = new Size(320, 210), MaximizeBox = false };
+                TextBox updname = new TextBox() { Location = new Point(110, 10), Text = dgvStudents.SelectedRows[0].Cells["Name"].Value.ToString() };
+                Label upnamelb = new Label() { Location = new Point(10, 10), Text = "Новое имя студента: " };
+                TextBox updspec = new TextBox() { Location = new Point(110, 40), Text = dgvStudents.SelectedRows[0].Cells["Speciality"].Value.ToString() };
+                Label upspeclb = new Label() { Location = new Point(10, 40), Text = "Новая специальность студента: " };
+                TextBox updgroup = new TextBox() { Location = new Point(110, 70) , Text = dgvStudents.SelectedRows[0].Cells["Group"].Value.ToString() };
+                Label upgrouplb = new Label() { Location = new Point(10, 70), Text = "Новая группа студента: " };
+                Button editBut = new Button() { Location = new Point(10, 100), Text = "Обновить данные о студенте" };
+
+                updata.Controls.Add(updname);
+                updata.Controls.Add(updspec);
+                updata.Controls.Add(updgroup);
+                updata.Controls.Add(editBut);
+                updata.Controls.Add(upgrouplb);
+                updata.Controls.Add(upspeclb);
+                updata.Controls.Add(upnamelb);
+
+                string newName = updname.Text;
+                string newSpeciality = updspec.Text;
+                string newGroup = updgroup.Text;
+
+                editBut.Click += (s, args) =>
+                {
+
+                    logic.EditStudent(id, updname.Text, updspec.Text, updgroup.Text);
+                    UpdateStudentList();
+                    updata.Close();
+                };
+                updata.ShowDialog();
+            }
+        }
+
+        /// <summary>
+        /// Получить гистограмму (распределение по специальностям)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnShowHistogram_Click(object sender, EventArgs e)
+        {
+            var histogram = logic.GetSpecialityHistogram();
+
+            Form histogramForm = new Form();
+            histogramForm.Text = "Распределение студентов по специальностям";
+
+            Chart chart = new Chart();
+            chart.Dock = DockStyle.Fill;
+
+            Series series = new Series("SpecialityHistogram");
+            series.ChartType = SeriesChartType.Column;
+
+            foreach (var speciality in histogram)
+            {
+                series.Points.AddXY(speciality.Key, speciality.Value);
+            }
+
+            chart.Series.Add(series);
+            chart.ChartAreas.Add(new ChartArea());
+
+            histogramForm.Controls.Add(chart);
+            histogramForm.ShowDialog();
+        }
+
+        public void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
